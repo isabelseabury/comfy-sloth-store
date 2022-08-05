ffeed4301f5bc698e672fc7ee5e631f0b0057844
Isabel Seabury <444belle@gmail.com>
2022-08-05 15:08:56 -0400

    first commit

---
+++ src/pages/AboutPage.js
@@ -0,0 +1,58 @@
+import React from 'react'
+import styled from 'styled-components'
+import { PageHero } from '../components'
+import aboutImg from '../assets/hero-bcg.jpeg'
+
+const AboutPage = () => {
+  return (
+    <main>
+      <PageHero title='about' />
+      <Wrapper className="page section section-center">
+        <img src={aboutImg} alt="desk" />
+        <article>
+          <div className="title">
+            <h2>Our Story</h2>
+            <div className="underline"></div>
+          </div>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam
+            consequat tempor tortor, eu finibus ligula tristique et. Donec
+            viverra porta velit, sed varius velit porttitor nec. In at eros
+            dictum felis vehicula porta vel vel dolor. Duis rutrum vulputate
+            ligula at fringilla. Suspendisse eget quam iaculis, aliquam orci ut,
+            blandit.
+          </p>
+        </article>
+      </Wrapper>
+    </main>
+  );
+}
+
+const Wrapper = styled.section`
+  display: grid;
+  gap: 4rem;
+  img {
+    width: 100%;
+    display: block;
+    border-radius: var(--radius);
+    height: 500px;
+    object-fit: cover;
+  }
+  p {
+    line-height: 2;
+    max-width: 45em;
+    margin: 0 auto;
+    margin-top: 2rem;
+    color: var(--clr-grey-5);
+  }
+  .title {
+    text-align: left;
+  }
+  .underline {
+    margin-left: 0;
+  }
+  @media (min-width: 992px) {
+    grid-template-columns: 1fr 1fr;
+  }
+`
+export default AboutPage
